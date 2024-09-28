f363537bf63b87eee93280f3a2106f4cea65d297
Muhammad Rifqi Al Baqi Ananta <mralbaqiananta@gmail.com>
2024-09-28 12:11:05 +0700

    Add files via upload

---
+++ 01_Pengenalan_CPP_Bagian_1/Guided/Percobaan.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+ cout<<"saya lagi belajar bahasa C++ nih!!!"<<endl;
+ return 0;
+}
+
+
+// int main() {    
+//     float W, X, Y; 
+//     float Z;    
+//     X = 7; 
+//     Y = 3; 
+//     W = 1;    
+//     Z = (X + Y) / (Y + W);    
+//     cout << "nilai Z = " << Z << endl;  
+//     return 0;  
+// }  
+
+
+// int main() {
+//     double tot_pembelian, diskon;
+//     cout << "Total pembelian: Rp ";
+//     cin >> tot_pembelian;
+
+//     diskon = 0;
+
+//     if (tot_pembelian >= 100000){
+//         diskon = 0.05 * tot_pembelian;
+
+//     cout << "Besar diskon = Rp " << diskon << endl;
+//     }
+//     return 0;
+
+// }
+
+
+
+// int main() {
+//     int i = 1;  
+//     int jum;   
+//     cin >> jum;
+
+//     do {
+//         cout << "Baris ke-" << i << endl; 
+//         i++; 
+//     } while (i <= jum);
+//     return 0;
+// }
+
+
