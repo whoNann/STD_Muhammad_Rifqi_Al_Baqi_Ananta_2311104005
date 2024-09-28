51d0c284da68b5f3131715c15beed6d81ff32c94
Muhammad Rifqi Al Baqi Ananta <mralbaqiananta@gmail.com>
2024-09-28 09:12:06 +0700

    Add files via upload

---
+++ 01_Pengenalan_CPP_Bagian_1/TP/soal_1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+    string nama, nim;
+    cout << "Siapa nama anda? ";
+    cin >> nama;
+
+    cout << "Berapa nim anda? ";
+    cin >> nim;
+
+    cout << "Nama saya: " << nama << endl;
+    cout << "NIM saya: " << nim << endl;
+    
+    return 0;
+}
